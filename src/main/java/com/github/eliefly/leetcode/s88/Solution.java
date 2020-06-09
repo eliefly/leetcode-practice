7e6a5697f683ad2213e412f7e9a162a31eb6a387
eliefly <eliefly@163.com>
2020-06-10 00:39:55 +0800

    update

--- src/main/java/com/github/eliefly/leetcode/s88/Solution.java
+++ src/main/java/com/github/eliefly/leetcode/s88/Solution.java
@@ -2,10 +2,10 @@ package com.github.eliefly.leetcode.s88;
 
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int[] res = new int[m + n];
+        int[] res = new int[m];
         int i = 0, j = 0, k = 0;
-        while (i < m && j < n) {
-            if (i < m && nums1[i] <= nums2[j]) {
+        while (i < m - n && j < n) {
+            if (i < m - n && nums1[i] <= nums2[j]) {
                 res[k] = nums1[i];
                 ++i;
             } else {
@@ -14,12 +14,12 @@ class Solution {
             }
             ++k;
         }
-        while (i < m) {
+        while (i < m - n) {
             res[k++] = nums1[i++];
         }
         while (j < n) {
             res[k++] = nums2[j++];
         }
-        nums1 = res;
+        System.arraycopy(res, 0, nums1, 0, m);
     }
 }
