9d8facee3ee05719c435b905770a446035c9679c
eliefly <eliefly@163.com>
2020-06-16 00:07:53 +0800

    update

---
+++ src/main/java/com/github/eliefly/leetcode/s215/Solution2.java
@@ -0,0 +1,29 @@
+package com.github.eliefly.leetcode.s215;
+
+import java.util.PriorityQueue;
+
+/**
+ * 在未排序的数组中找到第 k 个最大的元素。请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: [3,2,1,5,6,4] 和 k = 2
+ * 输出: 5
+ * <p>
+ * 示例 2:
+ * <p>
+ * 输入: [3,2,3,1,2,4,5,5,6] 和 k = 4
+ * 输出: 4
+ */
+class Solution2 {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>((o1, o2) -> o1 - o2);
+        for (int num : nums) {
+            minHeap.add(num);
+            if (minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+        return minHeap.peek();
+    }
+}
