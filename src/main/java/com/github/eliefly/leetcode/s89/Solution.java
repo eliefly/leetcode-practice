15330f097fae6ed650b372be81d0a1369bb1bc89
eliefly <eliefly@qq.com>
2020-06-24 10:42:01 +0800

    update

---
+++ src/main/java/com/github/eliefly/leetcode/s89/Solution.java
@@ -0,0 +1,27 @@
+package com.github.eliefly.leetcode.s89;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+
+    /**
+     * 看答案做的
+     *
+     * @param n
+     * @return
+     */
+    public List<Integer> grayCode(int n) {
+        List<Integer> res = new ArrayList<Integer>() {{
+            add(0);
+        }};
+        int prfix = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = res.size() - 1; j >= 0; j--) {
+                res.add(res.get(j) + prfix);
+            }
+            prfix = prfix << 1;
+        }
+        return res;
+    }
+}
