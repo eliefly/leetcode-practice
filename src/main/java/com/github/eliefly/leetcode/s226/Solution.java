f4c10d0eeda80f8a7b244b51bd784fbddfeb6ae9
eliefly <eliefly@qq.com>
2020-07-16 10:02:48 +0800

    update

---
+++ src/main/java/com/github/eliefly/leetcode/s226/Solution.java
@@ -0,0 +1,54 @@
+package com.github.eliefly.leetcode.s226;
+
+import com.github.eliefly.leetcode.common.TreeNode;
+
+/**
+ * 翻转一棵二叉树。
+ * <p>
+ * 示例：
+ * <p>
+ * 输入：
+ * <p>
+ * 4
+ * /   \
+ * 2     7
+ * / \   / \
+ * 1   3 6   9
+ * <p>
+ * 输出：
+ * <p>
+ * 4
+ * /   \
+ * 7     2
+ * / \   / \
+ * 9   6 3   1
+ */
+class Solution {
+/*    public TreeNode invertTree(TreeNode root) {
+        invert(root);
+        return root;
+    }
+
+    private void invert(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+        invert(root.left);
+        invert(root.right);
+    }*/
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
--- src/main/java/com/github/eliefly/leetcode/s234/Solution1.java
+++ src/main/java/com/github/eliefly/leetcode/s234/Solution1.java
@@ -23,7 +23,59 @@ import com.github.eliefly.leetcode.common.ListNode;
 class Solution1 {
     // 空间复杂O(1)的方法
     public boolean isPalindrome(ListNode head) {
-
-        return true;
+        if (head == null) {
+            return true;
+        }
+        ListNode firstHalfTail = findMiddleNode(head);
+        // 反转后半部分节点
+        ListNode secondHalfHead = reverseList(firstHalfTail.next);
+        boolean res = true;
+        ListNode head1 = head;
+        ListNode head2 = secondHalfHead;
+        while (res && head2 != null) {
+            if (head1.val != head2.val) {
+                res = false;
+            }
+            head1 = head1.next;
+            head2 = head2.next;
+        }
+        // 再次反转，恢复原有节点次序
+        reverseList(secondHalfHead);
+        return res;
     }
+
+    /**
+     * 快慢指针查找链表的中间节点
+     *
+     * @param head
+     * @return
+     */
+    private ListNode findMiddleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    /**
+     * 反转链表
+     *
+     * @param head
+     * @return
+     */
+    private ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+
 }
