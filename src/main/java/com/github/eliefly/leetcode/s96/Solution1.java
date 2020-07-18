718238c4e66ba7967c18ace6947a548ea8eb942b
eliefly <eliefly@qq.com>
2020-07-18 11:39:01 +0800

    update

---
+++ src/main/java/com/github/eliefly/leetcode/s96/Solution1.java
@@ -0,0 +1,33 @@
+package com.github.eliefly.leetcode.s96;
+
+/**
+ * 给定一个整数 n，求以 1 ... n 为节点组成的二叉搜索树有多少种？
+ * <p>
+ * 示例:
+ * 输入: 3
+ * 输出: 5
+ * 解释:
+ * 给定 n = 3, 一共有 5 种不同结构的二叉搜索树:
+ * <p>
+ * 1         3     3      2      1
+ * \       /     /      / \      \
+ * 3     2     1      1   3      2
+ * /     /       \                 \
+ * 2     1         2                 3
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/unique-binary-search-trees
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+class Solution1 {
+    /**
+     * 事实上我们在方法一中推导出的 G(n)G(n)G(n)函数的值在数学上被称为卡塔兰数 CnC_nCn​。卡塔兰数更便于计算的定义如下:
+     */
+    public int numTrees(int n) {
+        long c = 1;
+        for (int i = 0; i < n; i++) {
+            c = c * 2 * (2 * i + 1) / (i + 2);
+        }
+        return (int) c;
+    }
+}
