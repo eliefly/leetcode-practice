33569a3c48899a7861607ac5580c1b20e4e84beb
eliefly <eliefly@qq.com>
2020-07-18 11:33:50 +0800

    update

--- src/main/java/com/github/eliefly/leetcode/s96/Solution2.java
+++ src/main/java/com/github/eliefly/leetcode/s96/Solution2.java
@@ -19,9 +19,18 @@ package com.github.eliefly.leetcode.s96;
  * 链接：https://leetcode-cn.com/problems/unique-binary-search-trees
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
-class Solution {
+class Solution2 {
+
+    // 动态规划
     public int numTrees(int n) {
-        // todo
-        return 0;
+        int[] g = new int[n + 1];
+        g[0] = 1;
+        g[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                g[i] += g[j - 1] * g[i - j];
+            }
+        }
+        return g[n];
     }
 }
